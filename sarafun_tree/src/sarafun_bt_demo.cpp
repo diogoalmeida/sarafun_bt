3f1662fd2e0c1f44f42a272fc74403ab7c1e8bcf
Diogo Almeida <diogoa@kth.se>
2016-07-14 09:52:33 +0200

    Removed code comments, turned the BT tick period into a parameter

--- sarafun_tree/src/sarafun_bt_demo.cpp
+++ sarafun_tree/src/sarafun_bt_demo.cpp
@@ -14,6 +14,7 @@ using namespace BT;
 int main(int argc, char **argv) {
   ros::init(argc, argv, "sarafun_bt_demo");
   std::string filename;
+  int TickPeriod_milliseconds = 0;
 
   if (ros::param::has("/sarafun/bt/file")) {
     ros::param::get("/sarafun/bt/file", filename);
@@ -21,41 +22,16 @@ int main(int argc, char **argv) {
     filename = std::string("example.json");
   }
 
+  if (ros::param::has("/sarafun/bt/tick_period")){
+	ros::param::get("/sarafun/bt/tick_period", TickPeriod_milliseconds);
+  } else {
+    TickPeriod_milliseconds = 1000;
+  }
+
   std::string path =
       ros::package::getPath("sarafun_tree") + "/data/" + filename;
   bt_parser::Parser parser(path);
   try {
-    int TickPeriod_milliseconds = 1000;
-
-    // ROSAction *grab_action = new ROSAction("grab_object_action");
-    // ROSAction *grab_action2 = new ROSAction("grab_object_action");
-    // ROSAction *approach_objects_action =
-    //     new ROSAction("approach_objects_action");
-    // ROSAction *folding_assembly_action =
-    //     new ROSAction("folding_assembly_action");
-    // ROSAction *insertion_with_deformation_action =
-    //     new ROSAction("insertion_with_deformation_action");
-    // ROSAction *place_action = new ROSAction("place_action");
-    //
-    // ROSCondition *condition = new ROSCondition("C1");
-    //
-    // // SelectorStarNode *selector1 = new SelectorStarNode("sel1");
-    // // SequenceStarNode *sequence1 = new SequenceStarNode("seq1");
-    //
-    // SelectorStarNode *selector1 = new SelectorStarNode("sel1");
-    // SequenceStarNode *sequence1 = new SequenceStarNode("seq1");
-    //
-    // // sequence1->GetType();
-    // sequence1->AddChild(grab_action);
-    // sequence1->AddChild(grab_action2);
-    // sequence1->AddChild(approach_objects_action);
-    // selector1->AddChild(folding_assembly_action);
-    // selector1->AddChild(insertion_with_deformation_action);
-    // // sequence1->AddChild(folding_assembly_action);
-    // // sequence1->AddChild(insertion_with_deformation_action);
-    // sequence1->AddChild(selector1);
-    // sequence1->AddChild(place_action);
-
     ControlNode *root = dynamic_cast<ControlNode *>(parser.parseTree());
 
     if (root != nullptr) {
